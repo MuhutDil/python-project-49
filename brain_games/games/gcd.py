7ca549f404d9a57f8fdca729890d693b9b981f79
Dil <m.dilyara2010@yandex.ru>
2025-07-08 08:21:02 +0300

    Step 7.0

---
+++ brain_games/games/gcd.py
@@ -0,0 +1,21 @@
+import math
+import random
+
+
+def rule():
+    print('Find the greatest common divisor of given numbers.')
+
+
+def generate_question():  # NOSONAR
+    first_num = random.randint(1, 100)
+    second_num = random.randint(1, 100)
+    return f'{first_num} {second_num}'
+
+
+def transform_math_expression(string_expression):
+    first, second = string_expression.split()
+    return int(first), int(second)
+
+
+def answer_to_question(question):
+    return str(math.gcd(*transform_math_expression(question)))
