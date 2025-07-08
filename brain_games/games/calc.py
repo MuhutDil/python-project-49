d02045bc133cae69881af0e1b3f2bffe937603b0
Dil <m.dilyara2010@yandex.ru>
2025-07-08 08:04:58 +0300

    Step 6.1

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -5,21 +5,25 @@ def rule():
     print('What is the result of the expression?')
 
 
-def generate_question():
+def generate_question():  # NOSONAR
     operators = ('+', '-', '*')
-    first_num = random.randint(1, 50)  # NOSONAR
-    second_num = random.randint(1, 50)  # NOSONAR
-    operator = random.choice(operators)  # NOSONAR
+    first_num = random.randint(1, 50)
+    second_num = random.randint(1, 50)
+    operator = random.choice(operators)
     return f'{first_num} {operator} {second_num}'
 
 
-def answer_to_question(question):
-    first, operator, second = question.split()
+def transform_math_expression(string_expression):
+    first, operator, second = string_expression.split()
     first_num, second_num = int(first), int(second)
     match operator:
         case '+':
-            return str(first_num + second_num)
+            return first_num + second_num
         case '-':
-            return str(first_num - second_num)
+            return first_num - second_num
         case '*':
-            return str(first_num * second_num)
+            return first_num * second_num
+
+
+def answer_to_question(question):
+    return str(transform_math_expression(question))
