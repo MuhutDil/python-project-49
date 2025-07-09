c59d83b288fbae3b026c93ae656ac95909a863fd
Dil <m.dilyara2010@yandex.ru>
2025-07-09 08:33:36 +0300

    Change names

--- brain_games/game_basis.py
+++ brain_games/game_basis.py
@@ -1,6 +1,6 @@
 import prompt
 
-LIMIT_ANSWERS = 3
+MAX_CORRECT_ANSWERS = 3
 
 
 def welcome_user():
@@ -29,18 +29,18 @@ def endind_game(name, status_game, *, answer_data):
 def game(rule, generate_question, answer_to_question):
     name = welcome_user()
     rule()
-    count_right_answer = 0
+    correct_answers = 0
     status_game = True
-    right_answer_data = {}
-    while count_right_answer < LIMIT_ANSWERS:
+    answer_data = {}
+    while correct_answers < MAX_CORRECT_ANSWERS:
         question = generate_question()
         user_answer = ask_question(question)
         correct_answer = answer_to_question()
         if correct_answer == user_answer:
-            count_right_answer += 1
+            correct_answers += 1
         else:
             status_game = False
-            right_answer_data['user_answer'] = user_answer
-            right_answer_data['correct_answer'] = correct_answer
+            answer_data['user_answer'] = user_answer
+            answer_data['correct_answer'] = correct_answer
             break
-    endind_game(name, status_game, answer_data=right_answer_data) 
+    endind_game(name, status_game, answer_data=answer_data) 
